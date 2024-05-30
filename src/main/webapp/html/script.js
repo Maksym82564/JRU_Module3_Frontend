78c8aaaa8c1e1c79acb8e87d947f047ee3220b43
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-05-30 18:33:59 +0300

    Add js logic for drop down list

--- src/main/webapp/html/script.js
+++ src/main/webapp/html/script.js
@@ -1,5 +1,14 @@
-function fillTable() {
-    $.get("rest/players?", (players) => {
+let playersCount = 0;
+let playersPerPage = 3;
+let playersAmount = 0;
+let currentPageNumber = 0;
+
+createAccountPerPageDropDown();
+fillTable(0, playersPerPage);
+getPlayersCount();
+
+function fillTable(pageNumber, pageSize) {
+    $.get(`rest/players?pageNumber=${pageNumber}&pageSize=${pageSize}`, (players) => {
         const $playersTableBody = $('.players-table-body')[0];
         let htmlRows = '';
         players.forEach((player) => {
@@ -15,7 +24,68 @@ function fillTable() {
                     <td class="character-cell">${player.banned}</td>
                 </tr>`
         })
+
+        Array.from($playersTableBody.children).forEach(row => row.remove());
+
         $playersTableBody.insertAdjacentHTML("beforeend",htmlRows);
     })
 }
-fillTable();
+
+function getPlayersCount() {
+    $.get("rest/players/count", (count) => {
+        playersCount = count;
+        updatePaginationButtons();
+    })
+}
+
+function updatePaginationButtons() {
+    if (playersCount === 0) {
+        playersAmount = 0;
+    }
+    else {
+        playersAmount = Math.ceil(playersCount / playersPerPage);
+    }
+
+    const $pageButtonsContainer = $('.pagination-buttons')[0];
+    const childButtonsCount = $pageButtonsContainer.children.length;
+
+    let paginationButtonHtml = '';
+
+    for (let i = 1; i < playersAmount; i++) {
+        paginationButtonHtml += `<button value="${i - 1}" class="pagination-button">
+                                    ${i}
+                                 </button>`;
+    }
+
+    if (childButtonsCount !== 0) {
+        Array.from($pageButtonsContainer.children).forEach(node => node.remove())
+    }
+
+    $pageButtonsContainer.insertAdjacentHTML("beforeend", paginationButtonHtml);
+}
+
+function createAccountPerPageDropDown() {
+    const $dropDown = $('.accounts-per-page')[0];
+    const options = createSelectOptions([3, 5, 10, 20], 3);
+    $dropDown.addEventListener('change', onPlayersPerPageChangeHandler);
+    $dropDown.insertAdjacentHTML('afterbegin', options);
+}
+
+function createSelectOptions(optionsArray, defaultValue) {
+    let optionHtml = '';
+
+    optionsArray.forEach(option => {
+        optionHtml +=
+            `<option ${defaultValue === option && 'selected'} value="${option}">
+                ${option}
+            </option>`;
+    })
+
+    return optionHtml;
+}
+
+function onPlayersPerPageChangeHandler(e) {
+    playersPerPage = e.currentTarget.value;
+    fillTable(currentPageNumber, playersPerPage);
+    updatePaginationButtons();
+}
